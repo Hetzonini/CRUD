5cee2e8dc34ffc80077cd75d732c82d81a7be251
Hetzonini <61172031+Hetzonini@users.noreply.github.com>
2021-01-07 16:08:16 -0600

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'bmsxw9gcgcndkmhjra08-mysql.services.clever-cloud.com',
+        user: 'ub6amz11bfog3kgh',
+        password: 'ZBOmGFQ418XJZ8bLBrbq',
+        database: 'bmsxw9gcgcndkmhjra08'
+    }
+
+};
